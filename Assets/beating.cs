ecdffddf2f598df4fe6b483d3832c52f4e9a5af0
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-11-01 14:59:03 +0200

    Beating.cs script

---
+++ Assets/beating.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class beating : MonoBehaviour
+{
+    
+ 
+    // double size_one = 1.5 ;
+    // double size_two = 0.2 ;
+    // double tmp  ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // tmp = Math.Sin(size_one) ;
+        //  transform.localScale = new Vector3((float)tmp,(float)tmp,0);
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        //  tmp = Math.Sin(size_two);
+        //  transform.localScale = new Vector3((float)tmp,(float)tmp,0);
+        // tmp = Math.Sin(size_one) ;
+        //  transform.localScale = new Vector3((float)tmp,(float)tmp,0);
+Vector3 vec = new Vector3(Mathf.Abs(Mathf.Sin(Time.time)), Mathf.Abs(Mathf.Sin(Time.time)), 0);
+ 
+         transform.localScale = vec;
+         Debug.Log("time: "+Time.time);
+    }
+}
