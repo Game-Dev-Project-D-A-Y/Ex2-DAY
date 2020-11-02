08262c56b455e59b97eee3a31822e8081bc13d86
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-11-02 14:52:20 +0200

    Add sheep

--- Assets/disappear.cs
+++ Assets/disappear.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 /**
-* This component will show and disappear the object everytime you'll click the mouse left button
+* This component will show and Disappear the object everytime you'll click the mouse left button
 */
-public class disappear : MonoBehaviour
+public class Disappear : MonoBehaviour
 {
+
+    float startX;
+    float startY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startX = transform.position.x;
+        startY = transform.position.y;
     }
 
     // Update is called once per frame
@@ -19,17 +23,17 @@ public class disappear : MonoBehaviour
 
           if(Input.GetMouseButtonDown(0)){
               if(transform.localScale.x == 0){
-                   Vector3 show = new Vector3(1,1,0);
+                   Vector3 show = new Vector3(startX,startY,0);
                    // Input.mousePosition ;
                     transform.localScale = new Vector3(1,1,0);   
                     transform.position = show ;  
-                            //Debug.Log("clicked just" + Input.mousePosition );
+                           // Debug.Log("clicked just" + Input.mousePosition );
                             return;
 
               }
          Vector3 disappear = new Vector3(0,0,0);
          transform.localScale = disappear;   
-       //  Debug.Log("clicked disapper");
+        // Debug.Log("clicked disapper");
              }  
 
          //By Up Arrow
