7fb8d62aceb166febb3fa55ac71fb058c2952187
alonp13 <alonp13@gmail.com>
2020-11-03 19:12:26 +0200

    Update Disappear.cs

--- Assets/Scripts/Disappear.cs
+++ Assets/Scripts/Disappear.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 */
 public class Disappear : MonoBehaviour
 {
-
     float startX;
+
     float startY;
+
     float scaleX;
+
     float scaleY;
-    
+
     void Start()
     {
         startX = transform.position.x;
@@ -24,38 +26,18 @@ public class Disappear : MonoBehaviour
     void Update()
     {
         // this function will work by mouse
-        
-          if(Input.GetMouseButtonDown(0)){
-              if(transform.localScale.x == 0){
-                   Vector3 show = new Vector3(startX,startY,0);
-                   // Input.mousePosition ;
-                    transform.localScale = new Vector3(scaleX,scaleY,0);   
-                    transform.position = show ;  
-                           // Debug.Log("clicked just" + Input.mousePosition );
-                            return;
-
-              }
-         Vector3 disappear = new Vector3(0,0,0);
-         transform.localScale = disappear;   
-        // Debug.Log("clicked disapper");
-             }  
-
-         //By Up Arrow
-
-        //           if(Input.GetKeyDown(KeyCode.UpArrow )){
-        //       if(transform.localScale.x == 0){
-        //            Vector3 show = new Vector3(1,1,0);
-        //            // Input.mousePosition ;
-        //             transform.localScale = new Vector3(1,1,0);   
-        //             transform.position = show ;  
-        //                     Debug.Log("clicked just" + Input.mousePosition );
-        //                     return;
-
-        //       }
-        //  Vector3 disappear = new Vector3(0,0,0);
-        //  transform.localScale = disappear;   
-        //  Debug.Log("clicked disapper");
-        //      }  
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (transform.localScale.x == 0)
+            {
+                Vector3 show = new Vector3(startX, startY, 0);
+                transform.localScale = new Vector3(scaleX, scaleY, 0);
+                transform.position = show;
+                return;
+            }
+            
+            Vector3 disappear = new Vector3(0, 0, 0);
+            transform.localScale = disappear;
+        }
     }
 }
